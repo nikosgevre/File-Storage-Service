f57947a896996cb8e2a9a867992820ed5aab38f6
nikos <nikos_gevre@hotmail.com>
2019-05-14 08:58:00 +0300

    Add files via upload

---
+++ package-installer.js
@@ -0,0 +1,32 @@
+// For installing the required packages
+// NOT including
+// npm install
+// and
+// npm install shelljs --save
+const shell = require('shelljs');
+
+shell.exec('npm install express --save');
+shell.exec('npm install nodemon --save-dev');
+shell.exec('npm install body-parser --save');
+shell.exec('npm install mongodb --save');
+shell.exec('npm install mongoose --save');
+
+shell.exec('npm install express-session --save');
+shell.exec('npm install connect-mongodb-session --save');
+shell.exec('npm install csurf --save');
+
+shell.exec('npm install connect-flash --save');
+
+shell.exec('npm install multer --save');
+shell.exec('npm install convertapi --save');
+shell.exec('npm install mkdirp --save');
+shell.exec('npm install rimraf --save');
+
+shell.exec('npm install express-validator --save');
+shell.exec('npm install bcryptjs --save');
+
+shell.exec('npm install @sendgrid/mail --save');
+
+shell.exec('npm install ejs --save');
+shell.exec('npm install jquery --save');
+shell.exec('npm install lightbox2 --save');
