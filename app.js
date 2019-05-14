5995bb450ad87739c34cf4321bc09fad185fad76
nikos <nikos_gevre@hotmail.com>
2019-05-14 09:30:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-    'mongodb+srv://nikosgevre:CPSChSeyPXstcxxa@cluster0-bkjt6.mongodb.net/eidiko';
+    'mongodb+srv://:@cluster0-bkjt6.mongodb.net/eidiko';
 
 const app = express();
 const store = new MongoDBStore({
